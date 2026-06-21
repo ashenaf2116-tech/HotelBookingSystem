a9a36a8f5b9ee454651d8bb85002ae0fcd0319b1
Dagimdirba383-bit <dagimdirba383@gmail.com>
2026-06-21 22:09:03 +0300

    Add Customer and Booking classes

---
+++ src/models/Booking.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Booking implements Serializable {
+    private int bookingId;
+    private Customer customer;
+    private Room room;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private double totalPrice;
+
+    public Booking(int bookingId, Customer customer, Room room, LocalDate checkIn, LocalDate checkOut) {
+        this.bookingId = bookingId;
+        this.customer = customer;
+        this.room = room;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.totalPrice = room.getPricePerNight() *
+                (checkOut.toEpochDay() - checkIn.toEpochDay());
+    }
+
+    public void displayInfo() {
+        System.out.println("Booking#" + bookingId + "|" + customer.getName() + " | Room" + room.getRoomNumber()
+                + " | Total:$" + totalPrice);
+    }
+}
---
+++ src/models/Customer.java
@@ -0,0 +1,50 @@
+package models;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    private int customerId;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Customer(int customerId, String name, String email, String phone) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void displayInfo() {
+        System.out.println("Customer: " + name + "| Email:" + email + " | Phone: " + phone);
+    }
+
+}
